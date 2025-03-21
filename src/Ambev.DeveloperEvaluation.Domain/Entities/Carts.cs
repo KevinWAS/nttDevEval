c0e3c6883f43074fffda64f650685ff0f1da25a6
Kevin Willians <kevin.willians.a.s@outlook.com>
2025-03-21 17:13:34 -0300

    Adds migration for Products and Carts

---
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/Carts.cs
@@ -0,0 +1,44 @@
+using Ambev.DeveloperEvaluation.Domain.Common;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities;
+
+
+/// <summary>
+/// Represents a Cart in the system with authentication and profile information.
+/// This entity follows domain-driven design principles and includes business rules validation.
+/// </summary>
+public class Carts : BaseEntity
+{
+    /// <summary>
+    /// UserId owner of the cart
+    /// </summary>
+    public Guid UserId { get; set; }
+
+    /// <summary>
+    /// Date of the cart
+    /// </summary>
+    public DateOnly Date { get; set; }
+
+    /// <summary>
+    /// List of products on this cart
+    /// </summary>
+    public List<Products>? Products { get; set; }
+
+    /// <summary>
+    /// Gets the date and time when the cart was created.
+    /// </summary>
+    public DateTime CreatedAt { get; set; }
+
+    /// <summary>
+    /// Gets the date and time of the last update to the Cart's information.
+    /// </summary>
+    public DateTime? UpdatedAt { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the Carts class.
+    /// </summary>
+    public Carts()
+    {
+        CreatedAt = DateTime.UtcNow;
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.ORM/Mapping/ProductsConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping;
+
+public class ProductsConfiguration : IEntityTypeConfiguration<Products>
+{
+    public void Configure(EntityTypeBuilder<Products> builder)
+    {
+        builder.ToTable("Products");
+
+        builder.HasKey(u => u.Id);
+        builder.Property(u => u.Id).HasColumnType("uuid").HasDefaultValueSql("gen_random_uuid()");
+        builder.Property(u => u.CartId).HasColumnType("uuid");
+        builder.Property(u => u.Title).IsRequired().HasMaxLength(100);
+        builder.Property(u => u.Description);
+        builder.Property(u => u.Price).HasColumnType("numeric");
+        builder.Property(u => u.ratingCount).HasColumnType("integer");
+        builder.Property(u => u.ratingRate).HasColumnType("numeric");
+        builder.Property(u => u.CreatedAt).HasColumnType("date");
+        builder.Property(u => u.UpdatedAt).HasColumnType("date");
+
+        builder.HasOne<Carts>()
+            .WithMany()
+            .HasForeignKey(u => u.CartId);
+
+    }
+}
